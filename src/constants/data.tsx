2766ff03d3e3f9fea43f67dab60628276f13ff14
Nemanja Malesija <malesija13@gmail.com>
2023-02-04 18:39:28 +0100

    Create data with images

---
+++ src/constants/data.tsx
@@ -0,0 +1,73 @@
+import Assasin from './images/assasin.jpg';
+import BirdPerson from './images/birdPerson.jpg';
+import Bully from './images/Bully.webp';
+import IceT from './images/IceT.jpg';
+import MrNimbus from './images/MrNimbus.webp';
+import PButhole from './images/PButhole.jpg';
+import Psychologist from './images/Psychologist.jpg';
+import ScarryTerry from './images/scary-terry.webp';
+import SuperHero from './images/SuperHero.webp';
+import YouDKme from './images/YouDKme.webp';
+
+export default [
+  {
+    id: 1,
+    name: 'Jaguar',
+    image: Assasin,
+    checked: false,
+  },
+  {
+    id: 2,
+    name: 'Frank Palicky',
+    image: Bully,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: 'Bird Person',
+    image: BirdPerson,
+    checked: false,
+  },
+  {
+    id: 4,
+    name: 'Ice T',
+    image: IceT,
+    checked: false,
+  },
+  {
+    id: 5,
+    name: 'Mr. Nimbus',
+    image: MrNimbus,
+    checked: false,
+  },
+  {
+    id: 6,
+    name: 'Puppy Butthole',
+    image: PButhole,
+    checked: false,
+  },
+  {
+    id: 7,
+    name: 'Dr. Wong',
+    image: Psychologist,
+    checked: false,
+  },
+  {
+    id: 8,
+    name: 'Scarry Terry',
+    image: ScarryTerry,
+    checked: false,
+  },
+  {
+    id: 9,
+    name: 'Planetina',
+    image: SuperHero,
+    checked: false,
+  },
+  {
+    id: 10,
+    name: 'Mrs.Panckakes',
+    image: YouDKme,
+    checked: false,
+  },
+];
