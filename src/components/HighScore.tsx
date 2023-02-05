3fc944f3c9a162f87da2d01dc8184fe76a1c54aa
Nemanja Malesija <malesija13@gmail.com>
2023-02-05 21:58:33 +0100

    Restyle score div

--- src/components/HighScore.tsx
+++ src/components/HighScore.tsx
@@ -5,8 +5,8 @@ const HighScore = () => {
   const { currentScore, highScore } = useGlobalContext();
   return (
     <section className="section section-high-score">
-      <p className="score score-current">Current score: {currentScore}</p>
-      <p className="score score-high">High score: {highScore}</p>
+      <div className="score score-current">Current score: {currentScore}</div>
+      <div className="score score-high">High score: {highScore}</div>
     </section>
   );
 };
